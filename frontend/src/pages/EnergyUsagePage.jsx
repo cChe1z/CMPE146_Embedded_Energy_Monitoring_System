c9469dc64877ab26b49d63bf1d00416b6c1b421c
Surya <suryakrish1123@gmail.com>
2026-04-18 21:41:03 -0700

    begin energy usage implementation

--- frontend/src/pages/EnergyUsagePage.jsx
+++ frontend/src/pages/EnergyUsagePage.jsx
@@ -1,4 +1,25 @@
+/* EnergyUsagePage.jsx */
+import styles from './css/EnergyUsage.module.css'
+
 function EnergyUsagePage() {
-  return <div style={{padding: '24px'}}><h1>Energy Usage</h1></div>
+  return(
+     <div className = {styles.page}>
+        <header className = {styles.header}>
+          <h1 className = {styles.title}>Energy Usage</h1>
+            <div className = {styles.status}>
+              <span className = {styles.dot}>●</span>
+              <span className = {styles.statusinfo}>Energy usage updated 15 seconds ago</span>
+            </div>
+        </header>
+        <div className = {styles.summary}>
+          <div className = {styles.decision}>
+          <button className = {styles.button}>Day</button>
+          <button className = {styles.button}>Week</button>
+          <button className = {`${styles.button} ${styles.selectedbutton}`}>Month</button>
+          </div>
+          <span className = {styles.date}>April 17, 2026</span>
+        </div>
+     </div>
+  )
 }
 export default EnergyUsagePage
